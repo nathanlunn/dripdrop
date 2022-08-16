83b6664b0dc108eb6fbd6ffd02ef3fdb9e4e9f75
Summer <nathandouglaslunn@gmail.com>
2022-08-16 13:13:41 -0400

    set up server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
 
+const app = express();
 const PORT = 8080;
+const { urlencoded } = require('express');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(urlencoded({extended: false}));
+app.use(cors());
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
-})
+});
+
+

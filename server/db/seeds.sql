1cec383458165ad084c43b9a85a2ff43dd05a576
Summer <nathandouglaslunn@gmail.com>
2022-08-16 12:49:48 -0400

    finish seeds for products

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -4,11 +4,11 @@ INSERT INTO users (email, name, password) VALUES ('summerjadedub@gmail.com', 'Su
 
 -- products seeds
 INSERT INTO products (name, description, price, stock) VALUES ('flash rubber soles','A pair of sneakers made with care. Sure to create envy in your hate-fans.', 79.99, 6);
-INSERT INTO products (name, description, price, stock) VALUES ();
-INSERT INTO products (name, description, price, stock) VALUES ();
-INSERT INTO products (name, description, price, stock) VALUES ();
-INSERT INTO products (name, description, price, stock) VALUES ();
-INSERT INTO products (name, description, price, stock) VALUES ();
+INSERT INTO products (name, description, price, stock) VALUES ('churn up hoddie', 'made with care for the care takers', 108.99, 15);
+INSERT INTO products (name, description, price, stock) VALUES ('break even more', 'profit is overrated', 119.99, 4);
+INSERT INTO products (name, description, price, stock) VALUES ('no sweat pants', "I don't workout but I don't don't workout..", 59.99, 20);
+INSERT INTO products (name, description, price, stock) VALUES ('all cap cap', "doesn't matter what I say you won't believe it", 34.99, 30);
+INSERT INTO products (name, description, price, stock) VALUES ('f*ck fit bit' "the only steps I'm keeping track of are the ones to financial freedom", 9.99, 40);
 
 -- categories seeds
 INSERT INTO categories (name) VALUES ('shoes');

ee64599754ebf0852ab4f26abefe61aaf3cf3841
Summer <nathandouglaslunn@gmail.com>
2022-09-22 11:41:41 -0400

    add components of confirming cart additions

--- client/src/Components/Product.js
+++ client/src/Components/Product.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/Product.scss';
 import cart from '../img/cart.png';
 import { useNavigate } from 'react-router-dom';
+import check from '../img/check.png';
+import cancel from '../img/cancel.png';
 
 export default function Product({product}) {
   const navigate = useNavigate();
+  const [addingToCart, setAddingToCart] = useState(false);
+  const [cartAmount, setCartAmount] = useState(1);
 
   const addToCart = () => {
 
@@ -20,7 +24,20 @@ export default function Product({product}) {
       </div>
       <div className='product__buttonDiv'>
         <button className='product__button product__button--buyNow' onClick={() => navigate('/purchase')}>BUY NOW</button>
-        <img src={cart} className='product__button product__button--addToCart' onClick={addToCart}></img>
+        {addingToCart && (
+          <div className='product__addingToCart'>
+            <button className='product__lessAmount' onClick={() => {
+                if (cartAmount > 0) {                  
+                  setCartAmount(cartAmount - 1)
+                }
+              }}>-</button>
+            <div className='product__cartAmount'>{cartAmount}</div>
+            <button className='product__moreAmount' onClick={() => {setCartAmount(cartAmount + 1)}}>+</button>
+            <img className='product__confirmCartAdd' src={check}></img>
+            <img className='product__cancelCartAdd' src={cancel} onClick={() => setAddingToCart(false)}></img>
+          </div>
+        )}
+        <img src={cart} className='product__button product__button--addToCart' onClick={() => setAddingToCart(true)}></img>
       </div>
     </div>
   )

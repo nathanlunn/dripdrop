4e3d9c38510b7191561b262bd967ed759d4009fd
Summer <nathandouglaslunn@gmail.com>
2022-09-22 10:53:12 -0400

    add BUY NOW and add to cart buttons to each product in Store

--- client/src/Components/Product.js
+++ client/src/Components/Product.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './styles/Product.scss';
+import cart from '../img/cart.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Product({product}) {
+  const navigate = useNavigate();
+
+  const addToCart = () => {
+
+  }
+
   return (
     <div className='product'>
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
-      <p>${product.price}.00</p>
-      {product.stock <= 5 && <p>ONLY {product.stock} left in stock!</p>}
-      
+      <div className='product__info'>
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
+        <p>${product.price}.00</p>
+        {product.stock <= 5 && <p>ONLY {product.stock} left in stock!</p>}
+      </div>
+      <div className='product__buttonDiv'>
+        <button className='product__button product__button--buyNow' onClick={() => navigate('/purchase')}>BUY NOW</button>
+        <img src={cart} className='product__button product__button--addToCart' onClick={addToCart}></img>
+      </div>
     </div>
   )
 }

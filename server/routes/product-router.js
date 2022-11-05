31faf6bdcae55d9dab314d8a16458ef8b35b8760
Summer <nathandouglaslunn@gmail.com>
2022-11-05 06:24:39 -0400

    start code for updating database to new cart amount of product for the logged in user, running into syntax error with code 42601

--- server/routes/product-router.js
+++ server/routes/product-router.js
@@ -17,12 +17,38 @@ router.get('/', (req, res) => {
 })
 
 router.post('/addcart', (req, res) => {
-  const cartAmount = req.body.cartAmount;
-  const productID = req.body.productID;
-  const userID = req.body.userID;
+  const cartAmount = parseInt(req.body.cartAmount);
+  const productID = parseInt(req.body.productID);
+  const userID = parseInt(req.body.userID);
   if (!userID) {
     res.send('no user');
   }
+  db.query('SELECT * FROM user_product_relation WHERE user_id = $1 AND product_id = $2', [userID, productID])
+  .then(res => {
+    if (res.rows.length) {
+      db.query('UPDATE user_product_relation SET status = "cart", product_quantity = product_quantity + $1 WHERE user_id = $2 AND product_id = $3 RETURNING *;', [cartAmount, userID, productID])
+      .then(res => {
+        console.log(res.rows);
+        return;
+      })
+      .catch(err => {
+        console.error(err.message);
+        return;
+      })
+    }
+    db.query('INSERT INTO user_product_relation (user_id, product_id, product_quantity) VALUES ($1, $2, $3);' [userID, productID, cartAmount])
+    .then(res => {
+      console.log(res.rows);
+      return;
+    })
+    .catch(err => {
+      console.error(err);
+      return;
+    })
+  })
+  .catch(err => {
+    console.error(err.message);
+  })
   res.send(`userID: ${userID}, productID: ${productID}, cartAmount: ${cartAmount} `);
 })
 
